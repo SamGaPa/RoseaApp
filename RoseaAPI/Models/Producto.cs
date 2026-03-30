c56f78ca9396380530e2cf9a9863af25ab5e502b
SamGaPa <231102@utags.edu.mx>
2026-03-29 22:41:10 -0600

    cambio de prueba

--- RoseaAPI/Models/Producto.cs
+++ RoseaAPI/Models/Producto.cs
@@ -8,7 +8,7 @@
             public string Descripcion { get; set; }
             public decimal Precio { get; set; }
             
-
+        // test
             public string? Imagen { get; set; }
             public decimal?  IdCategoria { get; set; }
              public string? NameCategoria { get; set; }
